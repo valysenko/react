a985c142a8b92a0f05d9f587ef340018d8f30f8a
valysenko95 <va_lysenko@ukr.net>
2016-03-03 15:40:45 +0200

    ES6: imports, arrow functions, template strings

--- github-notetaker-ES6/app/App.js
+++ github-notetaker-ES6/app/App.js
@@ -1,9 +1,9 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Router = require('react-router').Router;
-var routes = require('./config/routes');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router } from 'react-router';
+import routes from './config/routes';
 
 ReactDOM.render(
     <Router>{routes}</Router>,
     document.getElementById('app')
-);
+)
