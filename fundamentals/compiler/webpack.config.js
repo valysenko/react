11e5f38fc2e27afabfa503d186a255c73c221736
valysenko95 <va_lysenko@ukr.net>
2016-03-04 13:42:41 +0200

    JSX Live compiler to React Code

---
+++ fundamentals/compiler/webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: "./App.js",
+    output: {
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
+    }
+};
